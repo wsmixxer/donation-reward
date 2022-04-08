476428f32afebd56074aaf596c0b5d85db1b1d51
bkensey <bkensey@gmail.com>
2022-04-08 11:02:46 -0400

    feat: :sparkles: Add support for relay control
    This basic feature allows the device to sense a hightened level of light and trigger a burst pattern on the relay, which, in our case, drives our propane fire poofer.

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,55 @@
 #define PIN 6
 #define ANALOG_PIN A0
 #define LED_PIN 4
+#define RELAY_PIN 13
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
   Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
 
   strip.begin();
-  strip.setBrightness(50);
+  strip.setBrightness(100);
+  strip.setPixelColor(0, strip.Color(0, 0, 255));
   strip.show();
 }
 
 void loop() {
   int A = analogRead(ANALOG_PIN);
   Serial.println(A);// Display serial results in serial monitor.
-  if (A > 400 ) {   // 800 seems to be the appropriate cutoff for sensing DipJar led light output
-    // Activated state
+  if (A > 800 ) {   // 800 seems to be the appropriate cutoff for sensing DipJar led light output
+    // Active state
     strip.setPixelColor(0, strip.Color(0, 255, 0));
     strip.show();
-  }
 
-  else {
-    // Ready state
-        strip.setPixelColor(0, strip.Color(0, 0, 255));
+    // Relay burst pattern
+    digitalWrite(RELAY_PIN, HIGH);
+    delay(600);
+    digitalWrite(RELAY_PIN, LOW);
+
+    delay(300);
+
+    digitalWrite(RELAY_PIN, HIGH);
+    delay(300);
+    digitalWrite(RELAY_PIN, LOW);
+
+    delay(300);
+
+    digitalWrite(RELAY_PIN, HIGH);
+    delay(1200);
+    digitalWrite(RELAY_PIN, LOW);
+
+    // Stop listening for the next 5 seconds
+    strip.setPixelColor(0, strip.Color(255, 0, 0));
+    strip.show();
+    digitalWrite(RELAY_PIN, LOW);
+    delay(30000);
+    
+    // Return to ready state
+
+    strip.setPixelColor(0, strip.Color(0, 0, 255));
     strip.show();
   }
 }
